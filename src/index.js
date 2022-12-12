2f885416c82bc5ca74407f5432647d287d47a1d2
Terry Roe <terryroe@gmail.com>
2022-12-12 13:55:06 -0700

    GRMA-19 Initial cleanup and app output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
