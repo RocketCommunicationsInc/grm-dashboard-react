cc2bfb3125803461cde9efc456de8a6c2717fe3e
Brian <brian@briankiernan.com>
2022-12-19 13:26:46 -0500

    feature(layout): create initial layout with css grid and included scaffold tabs bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import './App.scss';
-import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
+import { RuxTab, RuxTabs } from '@astrouxds/react';
 
-function App() {
+import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
+import './App.scss';
+
+const App = () => {
   return (
     <>
       <GlobalStatusBar />
+      <main className='Dashboard-grid'>
+        <aside className='Dashboard-grid__left-panel'>Left Panel</aside>
+        <nav className='Dashboard-grid__tabs-bar'>
+          <RuxTabs small>
+            <RuxTab selected>Contacts</RuxTab>
+            <RuxTab>Equipment</RuxTab>
+          </RuxTabs>
+        </nav>
+        <section className='Dashboard-grid__right-top-panel'>
+          Right Top Panel
+        </section>
+        <section className='Dashboard-grid__right-bottom-panel'>
+          Right Bottom Panel
+        </section>
+      </main>
     </>
   );
-}
+};
 
 export default App;
