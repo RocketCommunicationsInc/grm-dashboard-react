f875f751b5e46eadfbc882c04f0fde8f7f586357
Brian Kiernan <58487542+brikiernan@users.noreply.github.com>
2022-12-20 15:15:37 -0500

    feature(tabs): add tabs selected state (#3)
    * feature(layout): create initial layout with css grid and included scaffold tabs bar
    
    * fix(grid): adjust tabs background
    
    * feature(tabs): set up tabs state handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,39 @@
+import { useState } from 'react';
 import { RuxTab, RuxTabs } from '@astrouxds/react';
 
 import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import './App.scss';
 
 const App = () => {
+  const [tab, setTab] = useState('Contacts');
+
   return (
     <>
       <GlobalStatusBar />
       <main className='Dashboard-grid'>
-        <aside className='Dashboard-grid__left-panel'></aside>
+        <aside className='Dashboard-grid__left-panel'>
+          Alerts Left Panel Component Here
+        </aside>
         <nav className='Dashboard-grid__tabs-bar'>
-          <RuxTabs small>
-            <RuxTab selected>Contacts</RuxTab>
-            <RuxTab>Equipment</RuxTab>
+          <RuxTabs small onRuxselected={(e) => setTab(e.detail.innerText)}>
+            <RuxTab selected={tab === 'Contacts'}>Contacts</RuxTab>
+            <RuxTab selected={tab === 'Equipment'}>Equipment</RuxTab>
           </RuxTabs>
         </nav>
-        <section className='Dashboard-grid__right-top-panel'></section>
-        <section className='Dashboard-grid__right-bottom-panel'></section>
+        <section className='Dashboard-grid__right-top-panel'>
+          {tab === 'Contacts' ? (
+            <div>Contacts Right Top Panel Component Here</div>
+          ) : (
+            <div>Equipment Right Top Panel Component Here</div>
+          )}
+        </section>
+        <section className='Dashboard-grid__right-bottom-panel'>
+          {tab === 'Contacts' ? (
+            <div>Contacts Right Bottom Panel Component Here</div>
+          ) : (
+            <div>Equipment Right Bottom Panel Component Here</div>
+          )}
+        </section>
       </main>
     </>
   );
