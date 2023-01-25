2b67cdf0479b0adde495fbc4d315968147db08dc
atulmri123 <atul.murali@rocketcom.com>
2023-01-24 20:51:19 -0800

    Modified styling for AlertDetails, switched leftmost table to input fields instead

--- src/components/AlertsPanel/AlertDetails.js
+++ src/components/AlertsPanel/AlertDetails.js
@@ -4,7 +4,7 @@ import {
   RuxTableBody,
   RuxTableRow,
   RuxTableCell,
-  RuxTextarea,
+  RuxInput,
 } from '@astrouxds/react';
 import PanelHeader from '../../common/PanelHeader/PanelHeader';
 import useAlertsPanel from './useAlertsPanel';
@@ -22,45 +22,47 @@ const AlertDetails = ({ changeView, currentRow }) => {
       <section className='Alert-details-grid__top-panel'>
         <PanelHeader heading={'Alert Details'} />
 
-        <div className='Alert-details__container'>
+        <div className='Alert-details__sub-grid'>
           <div className='Alert-details__general-details'>
-            <p className='Alert-details__table-heading'>General information</p>
-            <div className='Alert-details__table-container'>
-              <RuxTable>
-                <RuxTableBody>
-                  <RuxTableRow>
-                    <RuxTableCell>Severity</RuxTableCell>
-                    <RuxTableCell>Critical</RuxTableCell>
-                  </RuxTableRow>
+            <RuxInput
+              className='Alert-details__input'
+              label='Severity'
+              value='Critical'
+              readonly={true}
+            />
 
-                  <RuxTableRow>
-                    <RuxTableCell>Alert ID</RuxTableCell>
-                    <RuxTableCell>Alert 123</RuxTableCell>
-                  </RuxTableRow>
+            <RuxInput
+              className='Alert-details__input'
+              label='Alert ID'
+              value='Alert 123'
+              readonly={true}
+            />
 
-                  <RuxTableRow>
-                    <RuxTableCell>Category</RuxTableCell>
-                    <RuxTableCell>ABC</RuxTableCell>
-                  </RuxTableRow>
+            <RuxInput
+              className='Alert-details__input'
+              label='Category'
+              value='CatABC'
+              readonly={true}
+            />
 
-                  <RuxTableRow>
-                    <RuxTableCell>Time</RuxTableCell>
-                    <RuxTableCell>07:21:19</RuxTableCell>
-                  </RuxTableRow>
-                </RuxTableBody>
-              </RuxTable>
-            </div>
-          </div>
-          <div className='Alert-details__description'>
-            <p className='Alert-details__table-heading'>Description</p>
-            <RuxTextarea
-              value='Lorem ipsum dolor...'
-              disabled={true}
-              size='large'
+            <RuxInput
+              className='Alert-details__input'
+              label='Time'
+              value='07:12:23'
+              readonly={true}
             />
           </div>
+          <div className='Alert-details__description'>
+            <p className='Alert-details__heading'>Description</p>
+            <div className='Alert-details__description-text'>
+              Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum
+              dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem
+              ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
+              Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor...
+            </div>
+          </div>
           <div className='Alert-details__affected-contacts'>
-            <p className='Alert-details__table-heading'>Affected Contacts</p>
+            <p className='Alert-details__heading'>Affected Contacts (##)</p>
 
             <div className='Alert-details__table-container'>
               <RuxTable>
@@ -84,10 +86,17 @@ const AlertDetails = ({ changeView, currentRow }) => {
           </div>
         </div>
         <div className='Alert-details__actions'>
-          <RuxButton secondary onClick={() => handleClick('main', currentRow)}>
+          <RuxButton
+            size='small'
+            secondary
+            onClick={() => handleClick('main', currentRow)}
+          >
             Dismiss
           </RuxButton>
-          <RuxButton onClick={() => handleClick('main', currentRow)}>
+          <RuxButton
+            size='small'
+            onClick={() => handleClick('main', currentRow)}
+          >
             Acknowledge
           </RuxButton>
         </div>
