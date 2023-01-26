d0731deead4f2af1ec046df38a5fa162ad015659
Brian <brian@briankiernan.com>
2023-01-26 11:07:51 -0500

    create breadcrumb nav component

---
+++ src/components/BreadcrumbNav/BreadcrumbNav.js
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import './BreadcrumbNav.scss';
+
+const BreadcrumbNav = ({ links }) => (
+  <nav className='Breadcrumb-nav'>
+    {links.map(({ href, title }, i) => {
+      const last = links.length === i + 1;
+
+      return (
+        <Fragment key={title + i}>
+          {!last ? (
+            <>
+              <a href={href}>{title}</a>
+              <span> / </span>
+            </>
+          ) : (
+            <span>{title}</span>
+          )}
+        </Fragment>
+      );
+    })}
+  </nav>
+);
+
+export default BreadcrumbNav;
