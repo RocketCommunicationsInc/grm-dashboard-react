2d94e998a0eb85d2afbf819bb5087d690511b2cb
atulmri123 <110487512+atulmri123@users.noreply.github.com>
2023-01-27 09:55:34 -0800

    GRMA-53: Alert Details separate page (#22)
    * Added AlertDetails separate view, including grid and conditional rendering based on selected view
    
    * Implemented disable/ack alerts in AlertDetails.js
    
    * Added necessary tables/elements, styling remains
    
    * Modified styling for AlertDetails, switched leftmost table to input fields instead
    
    * Adjusted grid widths to look more like wireframe
    
    * Dynamic alerts info, styling changes
    
    * Moved labels to left of input fields
    
    * Replaced RuxTable with <ul>, <li>, added some styles from AlertsPanel.scss
    
    * Minor tweaks before merge

--- src/components/AlertsPanel/AlertsPanelItem.js
+++ src/components/AlertsPanel/AlertsPanelItem.js
@@ -1,10 +1,16 @@
+import { RuxButton } from '@astrouxds/react';
 import { flexRender } from '@tanstack/react-table';
 import { Fragment } from 'react';
 
 import { useDisclosure } from '../../hooks/useDisclosure';
 
-const AlertsPanelItem = ({ row }) => {
+const AlertsPanelItem = ({ row, changeView, setCurrentRow }) => {
   const { getDisclosureProps, getButtonProps } = useDisclosure();
+
+  const handleClick = (page, row) => {
+    changeView(page);
+    setCurrentRow(row);
+  };
 
   return (
     <li {...getButtonProps()}>
@@ -17,6 +23,16 @@ const AlertsPanelItem = ({ row }) => {
       </div>
       <div className='Alerts-panel__details' {...getDisclosureProps()}>
         {row.original.longMessage}
+
+        <div className='Alerts-panel__investigate-button'>
+          <RuxButton
+            onClick={() => {
+              handleClick('alertDetailsPage', row);
+            }}
+          >
+            Investigate
+          </RuxButton>
+        </div>
       </div>
     </li>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -4,42 +4,62 @@ import EquipmentStatusPanel from './EquipmentStatusPanel/EquipmentStatusPanel';
 import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import ContactsSummaryPanel from './ContactsSummaryPanel/ContactsSummaryPanel';
 import AlertsPanel from './AlertsPanel/AlertsPanel';
+import AlertDetails from './AlertsPanel/AlertDetails';
 import CurrentContactsPanel from './CurrentContactsPanel/CurrentContactsPanel';
 import './App.scss';
 
 const App = () => {
   const [tab, setTab] = useState('Contacts');
+  const [currentView, setCurrentView] = useState('main');
+  const [currentRow, setCurrentRow] = useState({});
 
-  return (
-    <>
-      <GlobalStatusBar />
-      <main className='Dashboard-grid'>
-        <aside className='Dashboard-grid__left-panel'>
-          <AlertsPanel />
-        </aside>
-        <nav className='Dashboard-grid__tabs-bar'>
-          <RuxTabs small onRuxselected={(e) => setTab(e.detail.innerText)}>
-            <RuxTab selected={tab === 'Contacts'}>Contacts</RuxTab>
-            <RuxTab selected={tab === 'Equipment'}>Equipment</RuxTab>
-          </RuxTabs>
-        </nav>
-        <section className='Dashboard-grid__right-top-panel'>
-          {tab === 'Contacts' ? (
-            <CurrentContactsPanel />
-          ) : (
-            <EquipmentStatusPanel />
-          )}
-        </section>
-        <section className='Dashboard-grid__right-bottom-panel'>
-          {tab === 'Contacts' ? (
-            <ContactsSummaryPanel />
-          ) : (
-            <div>Equipment Right Bottom Panel Component Here</div>
-          )}
-        </section>
-      </main>
-    </>
-  );
+  switch (currentView) {
+    case 'alertDetailsPage':
+      return (
+        <>
+          <GlobalStatusBar />
+          <AlertDetails
+            changeView={(view) => setCurrentView(view)}
+            currentRow={currentRow}
+          />
+        </>
+      );
+
+    default:
+      return (
+        <>
+          <GlobalStatusBar />
+          <main className='Dashboard-grid'>
+            <aside className='Dashboard-grid__left-panel'>
+              <AlertsPanel
+                changeView={(view) => setCurrentView(view)}
+                setCurrentRow={(row) => setCurrentRow(row)}
+              />
+            </aside>
+            <nav className='Dashboard-grid__tabs-bar'>
+              <RuxTabs small onRuxselected={(e) => setTab(e.detail.innerText)}>
+                <RuxTab selected={tab === 'Contacts'}>Contacts</RuxTab>
+                <RuxTab selected={tab === 'Equipment'}>Equipment</RuxTab>
+              </RuxTabs>
+            </nav>
+            <section className='Dashboard-grid__right-top-panel'>
+              {tab === 'Contacts' ? (
+                <CurrentContactsPanel />
+              ) : (
+                <EquipmentStatusPanel />
+              )}
+            </section>
+            <section className='Dashboard-grid__right-bottom-panel'>
+              {tab === 'Contacts' ? (
+                <ContactsSummaryPanel />
+              ) : (
+                <div>Equipment Right Bottom Panel Component Here</div>
+              )}
+            </section>
+          </main>
+        </>
+      );
+  }
 };
 
 export default App;
