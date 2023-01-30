58a58b008543cf232aed0bf34667883270816936
Brian <brian@briankiernan.com>
2023-01-30 11:10:29 -0500

    move alerts and contact detials to their own dir

---
+++ src/components/AlertDetails/AlertDetails.js
@@ -0,0 +1,114 @@
+import { RuxButton, RuxInput } from '@astrouxds/react';
+
+import { capitalize, formatReadableTime } from '../../util/util';
+import useAlertsPanel from '../AlertsPanel/useAlertsPanel';
+import {
+  DetailsCommonGrid,
+  PanelBody,
+  PanelContainer,
+  PanelFooter,
+  PanelHeader,
+} from '../../common';
+import './AlertDetails.scss';
+
+const AlertDetails = ({ currentRow, setPage }) => {
+  const { dismissAcknowledgeAlerts } = useAlertsPanel();
+
+  const handleClick = () => {
+    dismissAcknowledgeAlerts(currentRow);
+    setPage('dashboard');
+  };
+
+  return (
+    <PanelContainer>
+      <PanelHeader heading='Alert Details' />
+
+      <PanelBody>
+        <DetailsCommonGrid>
+          <div className='Alert-details__general-details'>
+            <RuxInput
+              className='Alert-details__input'
+              label='Severity'
+              value={capitalize(currentRow.original.errorSeverity)}
+              readonly
+              size='small'
+            />
+
+            <RuxInput
+              className='Alert-details__input'
+              label='Alert ID'
+              value={currentRow.original.errorMessage.split(' - ')[0]}
+              readonly
+              size='small'
+            />
+
+            <RuxInput
+              className='Alert-details__input'
+              label='Category'
+              value={capitalize(currentRow.original.errorCategory)}
+              readonly
+              size='small'
+            />
+
+            <RuxInput
+              className='Alert-details__input'
+              label='Time'
+              value={formatReadableTime(currentRow.original.errorTime)}
+              readonly
+              size='small'
+            />
+          </div>
+
+          <div className='Alert-details__description-container'>
+            <p className='Alert-details__heading'>Description</p>
+            <div className='Alert-details__description-text'>
+              {currentRow.original.longMessage}
+              <br />
+              <br />
+              Lorem sit incididunt id occaecat irure. Lorem sit incididunt id
+              occaecat irure. Lorem sit incididunt id occaecat irure. Lorem sit
+              incididunt id occaecat irure. Lorem sit incididunt id occaecat
+              irure. Lorem sit incididunt id occaecat irure. Lorem sit
+              incididunt id occaecat irure. Lorem sit incididunt id occaecat
+              irure.
+            </div>
+          </div>
+
+          <div className='Alert-details__affected-contacts'>
+            <p className='Alert-details__heading'>Affected Contacts</p>
+
+            <div className='Alert-details__table-heading-container'>
+              <div className='Alert-details__table-heading'>Iron (number)</div>
+              <div className='Alert-details__table-heading'>GS (number)</div>
+              <div className='Alert-details__table-heading'>REV (number)</div>
+            </div>
+
+            <ul className='Alert-details__table-rows'>
+              <li>
+                <div>1</div> <div>1234</div> <div>12</div>
+              </li>
+              <li>
+                <div>2</div> <div>1234</div> <div>12</div>
+              </li>
+              <li>
+                <div>3</div> <div>1234</div> <div>12</div>
+              </li>
+              <li>
+                <div>4</div> <div>1234</div> <div>12</div>
+              </li>
+            </ul>
+          </div>
+        </DetailsCommonGrid>
+      </PanelBody>
+
+      <PanelFooter>
+        <RuxButton secondary onClick={handleClick}>
+          Dismiss
+        </RuxButton>
+        <RuxButton onClick={handleClick}>Acknowledge</RuxButton>
+      </PanelFooter>
+    </PanelContainer>
+  );
+};
+
+export default AlertDetails;
