88f29453d992619f2d636e4ea473839c13d983ff
Brian <brian@briankiernan.com>
2023-02-02 08:54:01 -0500

    change none component filenames to camelcase

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useReducer } from 'react';
 
-import { appReducer } from './AppReducer';
-import { initialState } from './AppInitialState';
+import { appReducer } from './appReducer';
+import { initialState } from './appInitialState';
 import { timeoutRepeater } from '../util/util';
 import { getRandomAlert, getRandomContact } from '../data/data';
 
