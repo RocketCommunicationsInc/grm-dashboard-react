b202bf94c8b07388e7c04077bc4a2e563530041f
Brian <brian@briankiernan.com>
2023-02-02 10:13:38 -0500

    refactor breadcrumb to use app state and dispatch

--- src/common/BreadcrumbNav/BreadcrumbNav.js
+++ src/common/BreadcrumbNav/BreadcrumbNav.js
@@ -1,23 +1,37 @@
 import { Fragment } from 'react';
+
+import { useAppContext } from '../../providers/AppProvider';
 import './BreadcrumbNav.scss';
 
-export const BreadcrumbNav = ({ links }) => (
-  <nav className='Breadcrumb-nav'>
-    {links.map(({ href, title }, i) => {
-      const last = links.length === i + 1;
+export const BreadcrumbNav = () => {
+  const { state, dispatch } = useAppContext();
 
-      return (
-        <Fragment key={title + i}>
-          {!last ? (
-            <>
-              <a href={href}>{title}</a>
-              <span> / </span>
-            </>
-          ) : (
-            <span>{title}</span>
-          )}
-        </Fragment>
-      );
-    })}
-  </nav>
-);
+  const handleClick = (e, page) => {
+    e.preventDefault();
+
+    dispatch({ type: 'SET_PAGE', payload: page });
+  };
+
+  return (
+    <nav className='Breadcrumb-nav'>
+      {state.links.map(({ href, page, title }, i) => {
+        const last = state.links.length === i + 1;
+
+        return (
+          <Fragment key={href + title}>
+            {!last ? (
+              <>
+                <a href={href} onClick={(e) => handleClick(e, page)}>
+                  {title}
+                </a>
+                <span> / </span>
+              </>
+            ) : (
+              <span>{title}</span>
+            )}
+          </Fragment>
+        );
+      })}
+    </nav>
+  );
+};
