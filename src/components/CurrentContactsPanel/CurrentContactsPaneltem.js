c05043993d65ebc3d2c519d950be874118c42e2f
Terry Roe <terryroe@gmail.com>
2023-02-06 14:49:46 -0700

    GRMA-64 Display ContactDetails when selecting from list of contacts

--- src/components/CurrentContactsPanel/CurrentContactsPaneltem.js
+++ src/components/CurrentContactsPanel/CurrentContactsPaneltem.js
@@ -1,19 +1,13 @@
-import { RuxOption, RuxSelect } from '@astrouxds/react';
 import { flexRender } from '@tanstack/react-table';
 import { randomContact } from '../../data/data';
-import { options } from '../../data/options';
 
-import { useDisclosure } from '../../hooks/useDisclosure';
 import { useAppContext } from '../../providers/AppProvider';
 import { randInt } from '../../util';
 
 const CurrentContactsPanelItem = ({ row }) => {
   const { dispatch } = useAppContext();
-  const { getDisclosureProps, getButtonProps } = useDisclosure();
 
-  const handleClick = (e) => {
-    e.preventDefault();
-
+  const handleClick = () => {
     dispatch({
       type: 'DISPLAY_CONTACT_DETAILS',
       payload: {
@@ -24,26 +18,11 @@ const CurrentContactsPanelItem = ({ row }) => {
   };
 
   return (
-    <li {...getButtonProps()} onClick={handleClick}>
+    <li onClick={handleClick}>
       <div className='Current-contacts-panel__item'>
         {row.getVisibleCells().map(({ id, column, getContext }) => (
           <div key={id}>{flexRender(column.columnDef.cell, getContext())}</div>
         ))}
-      </div>
-      <div
-        className='Current-contacts-panel__details'
-        {...getDisclosureProps()}
-      >
-        <div className='Current-contacts-panel__details--wrapper'>
-          <div>{row.original.contactDetail}</div>
-          <div className='Current-contacts-panel__details--selection'>
-            <RuxSelect label='Command Mode' size='small'>
-              {options.modes.map((mode) => (
-                <RuxOption key={mode} label={mode} value={mode} />
-              ))}
-            </RuxSelect>
-          </div>
-        </div>
       </div>
     </li>
   );
