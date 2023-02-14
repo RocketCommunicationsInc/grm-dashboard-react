1860ad37b1c23e8e74af4c5c112c4fc8eec68303
Paul Cristea <paul.cristea@rocketcom.com>
2023-02-13 17:33:26 -0800

    wip

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -10,28 +10,66 @@ import { appReducer } from './AppReducer';
 import { initialState } from './AppInitialState';
 import { randInt, timeoutRepeater } from '../util/util';
 import { getRandomAlert, getRandomContact, randomContact } from '../data/data';
+import { EgsMockApi } from '@pcatrocket/egs-mock-api';
 
 const AppContext = createContext({});
-
 export const useAppContext = () => useContext(AppContext);
+
+const mockApi = new EgsMockApi();
+mockApi.start();
+const connection = new WebSocket(mockApi.config.socket.url);
+
+connection.send(
+  JSON.stringify({
+    service: 'alert',
+    cmd: 'start',
+    minDelay: 5000,
+    maxDelay: 10000,
+  })
+);
+
+connection.send(
+  JSON.stringify({
+    service: 'contact',
+    cmd: 'start',
+    minDelay: 5000,
+    maxDelay: 10000,
+  })
+);
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(appReducer, initialState);
 
-  useEffect(() => {
-    return timeoutRepeater(() => {
-      if (state.alerts.length < 40) {
-        dispatch({ type: 'ADD_ALERT', payload: getRandomAlert() });
-      }
-    });
-  }, [state.alerts.length]);
-  useEffect(() => {
-    return timeoutRepeater(() => {
-      if (state.contacts.length < 40) {
-        dispatch({ type: 'ADD_CONTACT', payload: getRandomContact() });
-      }
-    });
-  }, [state.contacts.length]);
+  connection.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    const payload = data.data;
+
+    switch (data.service) {
+      case 'alert':
+        dispatch({ type: 'ADD_ALERT', payload });
+        break;
+      case 'contact':
+        dispatch({ type: 'ADD_CONTACT', payload });
+        break;
+      default:
+        throw Error(`Unknown service: ${data.service}`);
+    }
+  };
+
+  // useEffect(() => {
+  //   return timeoutRepeater(() => {
+  //     if (state.alerts.length < 40) {
+  //       dispatch({ type: 'ADD_ALERT', payload: getRandomAlert() });
+  //     }
+  //   });
+  // }, [state.alerts.length]);
+  // useEffect(() => {
+  //   return timeoutRepeater(() => {
+  //     if (state.contacts.length < 40) {
+  //       dispatch({ type: 'ADD_CONTACT', payload: getRandomContact() });
+  //     }
+  //   });
+  // }, [state.contacts.length]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
