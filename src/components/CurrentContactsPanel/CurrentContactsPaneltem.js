770219ae9384847941e6015b405dd543ce44d430
Joel Perez <joel@joelperez.dev>
2023-06-27 08:47:28 -0600

    add link to test route

--- src/components/CurrentContactsPanel/CurrentContactsPaneltem.js
+++ src/components/CurrentContactsPanel/CurrentContactsPaneltem.js
@@ -1,17 +1,23 @@
 import { flexRender } from '@tanstack/react-table';
+import { Link } from 'react-router-dom';
 
 import { useAppActions } from '../../providers/AppProvider';
 
 const CurrentContactsPanelItem = ({ row }) => {
   const { investigateContact } = useAppActions();
+  // console.log('row', row);
 
   return (
-    <li onClick={() => investigateContact(row.original)}>
-      <div className='Current-contacts-panel__item'>
-        {row.getVisibleCells().map(({ id, column, getContext }) => (
-          <div key={id}>{flexRender(column.columnDef.cell, getContext())}</div>
-        ))}
-      </div>
+    <li>
+      <Link to={`contacts/${row.original.contactId}`}>
+        <div className='Current-contacts-panel__item'>
+          {row.getVisibleCells().map(({ id, column, getContext }) => (
+            <div key={id}>
+              {flexRender(column.columnDef.cell, getContext())}
+            </div>
+          ))}
+        </div>
+      </Link>
     </li>
   );
 };
