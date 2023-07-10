b0bfbffa8f40dd31cf09f4fdc4e993e966cb21cc
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-07-10 16:36:42 -0600

    adding back filter logic although it doesn't work right now

--- src/common/EventLog/EventLog.tsx
+++ src/common/EventLog/EventLog.tsx
@@ -1,15 +1,25 @@
 import { RuxLog } from '@astrouxds/react';
 import { memo } from 'react';
 import { generateEvents } from '../../data/data';
+import { setHhMmSs } from '../../util';
 import './EventLog.css';
 
 export const EventLog = memo(() => {
   const events = generateEvents();
 
+  const filter = ''.toLowerCase();
+  const filteredEventLogs = events.filter((event) => {
+    return (
+      (event.message && event.message.toLowerCase().includes(filter)) ||
+      (event.timestamp &&
+        setHhMmSs(event.timestamp).toString().includes(filter))
+    );
+  });
+
   return (
     <div className='log-wrapper'>
       <RuxLog
-        data={events.map((event) => {
+        data={filteredEventLogs.map((event) => {
           const message =
             event.message.length < 62
               ? event.message
@@ -20,7 +30,7 @@ export const EventLog = memo(() => {
             message: message,
           };
         })}
-        filter=''
+        filter={filter}
       />
     </div>
   );
