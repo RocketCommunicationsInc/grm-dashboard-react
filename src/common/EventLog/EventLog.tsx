67f95d8a1c7634d51264c3450432556a0260fd52
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-07-10 15:48:01 -0600

    removed custom filter logic

--- src/common/EventLog/EventLog.tsx
+++ src/common/EventLog/EventLog.tsx
@@ -1,34 +1,15 @@
 import { RuxLog } from '@astrouxds/react';
-import { memo, useState, useMemo, SetStateAction } from 'react';
+import { memo } from 'react';
 import { generateEvents } from '../../data/data';
-import { setHhMmSs } from '../../util';
 import './EventLog.css';
 
 export const EventLog = memo(() => {
   const events = generateEvents();
-  const [filter, setFilter] = useState('');
-
-  const filteredEventLogs = useMemo(() => {
-    if (!filter) {
-      return events;
-    }
-    return events.filter((event) => {
-      return (
-        (event.message && event.message.toLowerCase().includes(filter)) ||
-        (event.timestamp &&
-          setHhMmSs(event.timestamp).toString().includes(filter))
-      );
-    });
-  }, [events, filter]);
-
-  const handleFilter = (e: { target: { value: SetStateAction<string> } }) => {
-    setFilter(e.target.value);
-  };
 
   return (
     <div className='log-wrapper'>
       <RuxLog
-        data={filteredEventLogs.map((event) => {
+        data={events.map((event) => {
           const message =
             event.message.length < 62
               ? event.message
