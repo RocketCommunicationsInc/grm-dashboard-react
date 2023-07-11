9d44c3a4de75773610bfa73a1a4c50147ed1750a
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-07-11 14:11:00 -0600

    add alert logic for acknowledge and dismiss, select one, select all

--- src/components/GlobalStatusBar/AlertPopUp/AlertPopUp.tsx
+++ src/components/GlobalStatusBar/AlertPopUp/AlertPopUp.tsx
@@ -1,4 +1,4 @@
-import { useTTCGRMAlerts } from '@astrouxds/mock-data';
+import { useTTCGRMAlerts, useTTCGRMActions } from '@astrouxds/mock-data';
 import {
   RuxButton,
   RuxCheckbox,
@@ -12,14 +12,41 @@ import {
   RuxTableRow,
 } from '@astrouxds/react';
 import { setHhMmSs } from '../../../util';
+import type { Alert } from '@astrouxds/mock-data';
+
 import './AlertPopUp.css';
 
 const AlertPopUp = () => {
   const { dataArray: alerts } = useTTCGRMAlerts();
+  const {
+    deleteAlertsWithProp,
+    anyAlertsHaveProp,
+    allAlertsHaveProp,
+    modifyAllAlerts,
+    modifyAlert,
+  } = useTTCGRMActions();
 
   const softwareAlerts = alerts.filter(
     (alert) => alert.category === 'software'
   );
+
+  const allSelected = allAlertsHaveProp('selected', true);
+  const selectAll = () => modifyAllAlerts({ selected: true });
+  const selectNone = () => modifyAllAlerts({ selected: false });
+  const anySelected = anyAlertsHaveProp('selected', true);
+  const deleteSelectedAlerts = () => deleteAlertsWithProp('selected', true);
+
+  const selectAllHandler = (e: CustomEvent) => {
+    const checkbox = e.target as HTMLRuxCheckboxElement;
+    if (checkbox.checked === true) {
+      selectAll();
+    } else {
+      selectNone();
+    }
+  };
+
+  const toggleSelected = (alert: Alert) =>
+    modifyAlert({ ...alert, selected: !alert.selected });
 
   return (
     <div className='pop-up-table-wrapper'>
@@ -27,7 +54,11 @@ const AlertPopUp = () => {
         <RuxTableHeader>
           <RuxTableHeaderRow>
             <RuxTableHeaderCell>
-              <RuxCheckbox />
+              <RuxCheckbox
+                onRuxchange={selectAllHandler}
+                checked={allSelected}
+                indeterminate={anySelected && !allSelected}
+              />
             </RuxTableHeaderCell>
             <RuxTableHeaderCell>Severity</RuxTableHeaderCell>
             <RuxTableHeaderCell>Alert ID</RuxTableHeaderCell>
@@ -38,7 +69,11 @@ const AlertPopUp = () => {
           {softwareAlerts.map((alert) => (
             <RuxTableRow>
               <RuxTableCell>
-                <RuxCheckbox />
+                <RuxCheckbox
+                  id={alert.id}
+                  checked={alert.selected}
+                  onRuxinput={() => toggleSelected(alert)}
+                />
               </RuxTableCell>
               <RuxTableCell>
                 <RuxStatus status={alert.status} />
@@ -50,8 +85,16 @@ const AlertPopUp = () => {
         </RuxTableBody>
       </RuxTable>
       <footer slot='footer'>
-        <RuxButton secondary>Dismiss</RuxButton>
-        <RuxButton>Acknowledge</RuxButton>
+        <RuxButton
+          onClick={deleteSelectedAlerts}
+          disabled={!anySelected}
+          secondary
+        >
+          Dismiss
+        </RuxButton>
+        <RuxButton onClick={deleteSelectedAlerts} disabled={!anySelected}>
+          Acknowledge
+        </RuxButton>
       </footer>
     </div>
   );
