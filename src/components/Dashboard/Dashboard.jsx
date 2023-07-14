b237bad975a9a35f650e560fca025c4dec435d2c
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-07-14 09:10:33 -0400

    move css and cleanup commented code

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import { RuxTab, RuxTabs, RuxTabPanel, RuxTabPanels } from '@astrouxds/react';
 import Alerts from '../AlertsPanel/Alerts';
 import TrendingEquipmentStatusPanel from '../TrendingEquipmentStatusPanel/TrendingEquipmentStatusPanel';
@@ -8,19 +7,13 @@ import ContactsSummaryPanel from '../ContactsSummaryPanel/ContactsSummaryPanel';
 import './Dashboard.css';
 
 const Dashboard = () => {
-  //const [tab, setTab] = useState('Contacts');
-
   return (
     <main className={`dashboard-page`}>
       <aside className='Dashboard-page__left-panel'>
         <Alerts />
       </aside>
       <nav className='Dashboard-page__tabs-bar'>
-        <RuxTabs
-          id='dashboard-tabs'
-          small
-          // onRuxselected={(e) => setTab(e.detail.innerText)}
-        >
+        <RuxTabs id='dashboard-tabs' small>
           <RuxTab id='contacts-tab'>Contacts</RuxTab>
           <RuxTab id='equipment-tab'>Equipment</RuxTab>
         </RuxTabs>
@@ -43,20 +36,6 @@ const Dashboard = () => {
           </section>
         </RuxTabPanel>
       </RuxTabPanels>
-      {/* <section className='Dashboard-page__right-top-panel'>
-        {tab === 'Contacts' ? (
-          <CurrentContactsTable />
-        ) : (
-          <EquipmentStatusPanel />
-        )}
-      </section>
-      <section className='Dashboard-page__right-bottom-panel'>
-        {tab === 'Contacts' ? (
-          <ContactsSummaryPanel />
-        ) : (
-          <TrendingEquipmentStatusPanel />
-        )}
-      </section> */}
     </main>
   );
 };
